c6dac3919b4273e2ba1c7d607b4317d7d8b54bfb
Christopher Penrose <cpenrose@steelcase.com>
2017-01-30 09:22:23 -0800

    upped scala version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-scalac "0.1.0"
+(defproject lein-scalac "0.1.1-SNAPSHOT"
   :eval-in-leiningen true
-  :dependencies [[org.scala-lang/scala-compiler "2.9.1"]
+  :dependencies [[org.scala-lang/scala-compiler "2.10.6"]
                  [lancet "1.0.1"]])
